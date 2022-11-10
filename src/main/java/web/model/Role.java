937e5e853871dcf663716dcdba0cad6ea2b51897
PavelBabkin <pavel.babkin21@gmail.com>
2022-11-10 11:22:55 +0600

    Commit "delete wildcard"

--- src/main/java/web/model/Role.java
+++ src/main/java/web/model/Role.java
@@ -4,7 +4,14 @@ package web.model;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 import java.util.List;
 
 @Entity
